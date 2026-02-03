a192f4281973b8d83ed15fec6d226ec8155a4bba
faizann111 <23311a0582@cse.sreenidhi.edu.in>
2026-02-03 15:38:52 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],  // ✅ matches all .html and .js files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
